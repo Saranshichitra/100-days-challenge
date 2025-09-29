46e4452bdb5f0e9216ebe90564205456a9b9b792
Saranshichitra <Saranshi.chitra@gmail.com>
2025-09-29 23:05:53 +0530

    Add files via upload

--- day16/program1.c
+++ day16/program1.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 int main() {
-    int num, binarynum = 0, remainder;                                         //Declaration and Initialization
+    int num, binarynum = 0, place = 1, remainder;                                         //Declaration and Initialization
     printf("Enter the number \n");
     scanf("%d", &num);                                                         //Accepting value from the user                
     while( num != 0) {
         remainder = num % 2;
-        if ( remainder == 0) {
-            binarynum = binarynum * 10  ;
-        }
-        else {
-          binarynum = binarynum * 10 + remainder ;
-        }
+         binarynum = binarynum + remainder * place;
+        place = place * 10;
         num = num / 2;
     } 
     printf("binary equivalant =%d", binarynum);                              //Output Statement   
     return 0;
-
 }
