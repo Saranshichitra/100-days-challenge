f3997990dfc36261783e2f109af33edba1c8e674
Saranshichitra <Saranshi.chitra@gmail.com>
2025-11-19 17:37:04 +0530

    Add files via upload

---
+++ day85/program1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+enum Values { A = 10, B, C};      //Declaration of enumeration
+int main(){
+   printf("A=%d\n", A);        
+   printf("B=%d\n", B);
+   printf("C=%d\n", C);
+   return 0;
+}
+
